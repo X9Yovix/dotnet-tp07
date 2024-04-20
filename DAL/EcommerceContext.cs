44175fb0187e4e4e64bd273948d930fd4f52c37a
Karim Ouazzou <karimouazzou@gmail.com>
2024-04-20 11:29:24 +0100

    Création du contexte de la base de données

---
+++ DAL/EcommerceContext.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Web;
+using TP07.Models;
+
+namespace TP07.DAL
+{
+    public class EcommerceContext : DbContext
+    {
+        public EcommerceContext() : base("EcommerceContext")
+        { }
+
+        public DbSet<Categorie> Categorie { get; set; }
+        public DbSet<Marque> Marque { get; set; }
+        public DbSet<Produit> Produit { get; set; }
+    }
+}
